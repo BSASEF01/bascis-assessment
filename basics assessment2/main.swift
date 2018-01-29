444de1385a81caf2c1ad92fd8284660d21394e72
Bersabeh Asefa <bersabehasefa@Interapts-MacBook-Pro-8.local>
2018-01-29 14:56:02 -0500

    finished assessment

--- basics assessment2/main.swift
+++ basics assessment2/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
+var number1: Double = 2
+var number2: Float = 2
+var sum = Float(number1) + number2
+
+//2. Create a constant called answer. Answer should be equal to a plus b minus c modulo 2
+var a = 2
+var b = 3
+var c = 4
+
+let a2 = 2
+let b2 = 3
+let c2 = 4
+print((a + b - c,  a2 + b2 + c2 ))
+//3. Create two variables called firstName and lastName. Create a third variable called fullname that is your first name and last name combined using string interpolation.
+//4. Using a comment list 2 rules for creating variables.
+//5. Using a for loop and an if statment print out every even number from 1 to 100
+
+var firstName = " Bersabeh "
+var lastName = "solomon "
+print (firstName + lastName)
+
+
+
+//don't ues emojy in the variable don't ues long wordes or space
+
+for i in 1...100 {
+    if i % 2 == 0 {
+        print( "Event")
+    }
+}
+
+
+
 
